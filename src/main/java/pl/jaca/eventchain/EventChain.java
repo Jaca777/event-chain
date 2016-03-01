f287ad6a39444bd81c2a9f87b4a9db8f5860aa62
Jaca777 <szymonrodant@gmail.com>
2016-03-02 00:28:26 +0100

    Initial commit

---
+++ src/main/java/pl/jaca/eventchain/EventChain.java
@@ -0,0 +1,14 @@
+package pl.jaca.eventchain;
+
+/**
+ * @author Jaca777
+ *         Created 2016-03-02 at 00
+ */
+public abstract class EventChain {
+    /**
+     *
+     * @param chain
+     */
+    public abstract void add(EventChain chain);
+    
+}
